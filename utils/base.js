047124d826c3e4fa39cf64136f1deab790dcac57
dudandan <1285912752@qq.com>
2020-10-19 14:53:28 +0800

    接口请求封装

---
+++ utils/base.js
@@ -0,0 +1,12 @@
+class Base {
+  constructor(){
+      
+  }
+  
+};
+
+Base.restUrl = 'http://62.234.27.2:8080/api-wx'; 
+// Config.restUrl = 'https://ylxp.guoquan.cn/api/';
+// Config.restUrl = 'https://ylxp.huadingyun.cn/api/';
+// Config.restUrl = 'http://39.107.78.168:9090/api/';    
+export { Base};
---
+++ utils/http.js
@@ -0,0 +1,56 @@
+import {
+  Base
+} from './base';
+
+class Http {
+  constructor() {
+
+  }
+  getNetworkType() {
+    return new Promise((resolve, reject) => {
+      wx.getNetworkType({
+        success(res) {
+          const networkType = res.networkType
+          console.log(res)
+          resolve(networkType)
+        }
+      })
+    })
+  }
+  // post方式请求数据的方法
+  sendPostRequest(url, data = {}) {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+
+    return new Promise((resolve, reject) => {
+      let userId=wx.getStorageSync('userId') || ""
+      wx.request({
+        url: Base.restUrl + url,
+        header: { 'content-type': 'application/json' },
+        method: 'POST',
+        data: userId ? {
+					userId,
+					...data
+				} : data,
+        success(res) {
+          // wx.hideLoading({})
+          resolve(res.data)
+        },
+        fail() {
+          // wx.hideLoading({})
+
+          reject('请求的接口错误')
+        },
+        complete: function () {
+          wx.hideLoading()
+        }
+      })
+    })
+  }
+
+}
+export {
+  Http
+};

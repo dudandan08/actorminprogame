ee7afcea5189b212f216fe369dd90d033a785184
whzh719 <whzh719@126.com>
2020-10-20 19:26:53 +0800

    艺人信息

--- pages/actorinfo/actorinfo.js
+++ pages/actorinfo/actorinfo.js
@@ -1,18 +1,51 @@
 // pages/actorAmount/actorinfo.js
+
+
+import {Http} from"../../utils/http";
+
+var util=require('../../utils/util');
+
+
+const app = getApp();
+
+const http = new Http();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+      actorsData:[]
 
+  },
+
+  loadData(){
+    wx.showLoading({
+      title: '加载中...',
+    })
+      let url= "/actor/info/"+app.globalData.user.userId;
+      console.log(url);
+      http.sendGetRequest(url,null).then(resp=>{
+        console.log("艺人信息 返回结果："+ JSON.stringify(resp) );
+        let result=resp.data;
+        if(result.code==200){
+          if(result.data!=null){
+            this.setData({
+              actorsData:result.data
+            })
+           
+          }
+        }
+        wx.hideLoading();
+      })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+  
   },
 
   /**
@@ -26,7 +59,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.loadData();
   },
 
   /**
